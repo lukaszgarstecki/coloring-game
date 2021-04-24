5fbba3f4192e39b97224352d07eeb8bb216cc30d
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2021-04-24 11:03:24 +0200

    Bootstrap project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ColoringGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
