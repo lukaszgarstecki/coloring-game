46a518c230d585cc5eef6252d8cd63ef06b9d573
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2021-05-18 21:32:07 +0200

    Publish trimmed executables

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -o release-linux-x64
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -o release-win-x64
+dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -p:PublishTrimmed=true -o release-linux-x64
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -p:PublishTrimmed=true -o release-win-x64
