06a97b449913e5b2db531ea5103c10d681cf6c31
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2021-05-18 19:38:52 +0200

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -o release-linux-x64-self-contained
 dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -o release-win-x64-self-contained
-dotnet publish -r linux-x64 -p:PublishSingleFile=true -p:PublishReadyToRun=false -o release-linux-x64
-dotnet publish -r win-x64 -p:PublishSingleFile=true -p:PublishReadyToRun=false -o release-win-x64
+dotnet publish -r linux-x64 -p:PublishSingleFile=true -o release-linux-x64
+dotnet publish -r win-x64 -p:PublishSingleFile=true -o release-win-x64
