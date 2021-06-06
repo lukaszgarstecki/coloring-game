9ba8481eba8e52112a574d690b86ab7f16941264
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2021-06-06 14:11:25 +0200

    Add non self contained exe build

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@
 
 dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -p:PublishTrimmed=true -o release-linux-x64
 dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -p:PublishTrimmed=true -o release-win-x64
+dotnet publish -r win-x64  -p:PublishTrimmed=true -o release-win-min-x64
