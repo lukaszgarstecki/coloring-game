bc9a7b718af9332c279a2f5c7a32aa3658780457
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2021-06-10 10:23:18 +0200

    Publish version with no runtime

--- publish.sh
+++ publish.sh
@@ -2,4 +2,5 @@
 
 dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -p:PublishTrimmed=true -o release-linux-x64
 dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -p:PublishReadyToRun=false -p:PublishTrimmed=true -o release-win-x64
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained false -p:PublishReadyToRun=false -o release-win-runtime-dependant-x64
 dotnet publish -r win-x64  -p:PublishTrimmed=true -o release-win-min-x64
